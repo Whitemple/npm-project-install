c9c005a3f42bf76066f85438774fea855f2bbb58
whitemple <yarikkhromey@mail.ru>
2024-09-18 19:17:43 +0300

    created some simply tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-function sum(a, b) {
-  return;
-  a + b;
+function consecutive(array) {
+    let count = 0;
+    for (let i = Math.min(...array); i<Math.max(...array); i++){
+        if(!array.includes(i)) count += 1
+    }
+    return count;
 }
-function minus(a, b) {
-  return a - b;
-}
-const ad = "fdfdgf";
 
-export { sum, minus };
+function high(string){
+    const intengerArray = [];
+    const tempArr = [];
+    const arr = string.split(' ');
+    arr.forEach(insideArray => {
+        insideArray.split('').forEach(word => {
+            tempArr.push(word.charCodeAt(0) - 96)
+        });
+        intengerArray.push(tempArr.reduce((acc, cur)=>acc + cur, 0))
+        tempArr.length = 0;
+    });
+    return arr[intengerArray.indexOf(Math.max(...intengerArray))]
+}
+function createPhoneNumber(numbers){
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+export { consecutive, high, createPhoneNumber };
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,15 @@
-import { sum, minus } from ".";
+import { consecutive, high, createPhoneNumber } from ".";
 
 describe("Descride basic functions", () => {
-  it("Testing Sum function", () => {
-    expect(2 + 2).toBe(4);
-    expect(5 + 7).toBe(12);
+  test("Testing Sum function", () => {
+    expect(consecutive([4, 8, 6])).toBe(2);
+    expect(consecutive([-1, -5])).not.toBe(2)
   });
+  test('Testing high function', () => {
+    expect(high('man i need a taxi up to ubud')).toEqual('taxi')
+  });
+  test('Testing createPhoneNumber function', () => {
+    expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual("(123) 456-7890")
+  })
+
 });
